d8d0477fccb43e24768705af4ad6c4a01f1f6949
luthfiybk <luthfiybk@gmail.com>
2024-08-08 12:30:50 +0700

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Luthfi Arya M on 08/08/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
